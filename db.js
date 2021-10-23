6252e498806d06b0f03a9476446115dc878100c8
Chirag The Doge <72273879+chirag350@users.noreply.github.com>
2021-10-24 00:06:07 +0530

    Update db.js

--- db.js
+++ db.js
@@ -4,8 +4,8 @@ export const db = mysql({
   config: {
     host: process.env.MYSQL_HOST,
     database: process.env.MYSQL_DBNAME,
-    user: MYSQL_USERNAME,
-    password: MYSQL_PASSWORD,
+    user: process.env.MYSQL_USERNAME,
+    password: process.env.MYSQL_PASSWORD,
   },
 })
 
